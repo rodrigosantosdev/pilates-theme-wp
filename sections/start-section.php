dbb74a49e658c57792cdefb8a6aa23b44ed4336e
rodrigosantosdev <rodrigofrontend@gmail.com>
2023-12-11 13:10:30 -0300

    feat: update images para webp e ajuste nos arquivo php

--- sections/start-section.php
+++ sections/start-section.php
@@ -1,5 +1,5 @@
 <section class="grid lg:grid-cols-2" id="start">
-  <div class="grid place-items-start md:place-items-center lg:place-content-center gap-4 p-8 lg:p-0">
+  <div class="grid gap-4 p-8 place-items-start md:place-items-center lg:place-content-center lg:p-0">
     <?php
     $args = array(
       'post_type' => 'pagina-start',
@@ -11,25 +11,25 @@
     if ($section_start_query->have_posts()) :
       while ($section_start_query->have_posts()) : $section_start_query->the_post();
     ?>
-    <h2 class="uppercase text-5xl"><?php the_field('title') ?></h2>
-    <p class="leading-loose break-words max-w-md font-light md:text-center"><?php the_field('description') ?></p>
+        <h2 class="text-5xl uppercase"><?php the_field('title') ?></h2>
+        <p class="max-w-md font-light leading-loose break-words md:text-center"><?php the_field('description') ?></p>
 
-    <a href="<?php the_field('reservar_esta_aula_agora') ?>" target="_blank" class="bg-lime-400 hover:bg-lime-500 rounded-full w-max px-6 py-3 cursor-pointer">
-      Reserva horário agora
-    </a>
-    <span class="font-light text-black-500 max-w-[180px] text-center">
-      <?php the_field('lugares') ?>
-    </span>
+        <a href="<?php the_field('reservar_esta_aula_agora') ?>" target="_blank" class="px-6 py-3 rounded-full cursor-pointer bg-lime-400 hover:bg-lime-500 w-max">
+          Reserva horário agora
+        </a>
+        <span class="font-light text-black-500 max-w-[180px] text-center">
+          <?php the_field('lugares') ?>
+        </span>
   </div>
-  <?php
+<?php
       endwhile;
       wp_reset_postdata(); // Restaura os dados originais do post.
     else :
 ?>
-  <p>Nenhuma página encontrada.</p>
-  <?php endif; ?>
+<p>Nenhuma página encontrada.</p>
+<?php endif; ?>
 
-  <div>
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/start.jpg" alt="">
-  </div>
+<div>
+  <img src="<?php echo get_template_directory_uri() ?>/assets/images/start.webp" alt="imagem de uma mulher fazendo exercicio" width="1000" height="667" loading="lazy">
+</div>
 </section>
--- sections/studio-section.php
+++ sections/studio-section.php
@@ -1,6 +1,6 @@
 <section class="grid lg:grid-cols-2" id="estudio">
   <div>
-    <img src="<?php echo get_template_directory_uri() ?>/assets/images/studio.jpg" alt="" class="w-full" />
+    <img src="<?php echo get_template_directory_uri() ?>/assets/images/studio.webp" alt="foto de um estudio" class="w-full" width="750" height="500" loading="lazy" />
   </div>
   <?php
   $args = array(
@@ -15,13 +15,13 @@
       $section_studio_query->the_post();
   ?>
 
-      <div class="grid place-content-evenly bg-black-950 text-black-50 p-8 lg:p-0">
-        <h2 class="uppercase text-5xl"><?php the_field('title') ?></h2>
-        <p class="leading-loose break-words max-w-sm font-light">
+      <div class="grid p-8 place-content-evenly bg-black-950 text-black-50 lg:p-0">
+        <h2 class="text-5xl uppercase"><?php the_field('title') ?></h2>
+        <p class="max-w-sm font-light leading-loose break-words">
           <?php the_field('description') ?>
         </p>
 
-        <a href="<?php the_field('link') ?>" target="_blank" class="bg-lime-400 text-lime-900 rounded-full w-max mt-4 hover:bg-lime-500 px-6 py-3 cursor-pointer">
+        <a href="<?php the_field('link') ?>" target="_blank" class="px-6 py-3 mt-4 rounded-full cursor-pointer bg-lime-400 text-lime-900 w-max hover:bg-lime-500">
           Mais sobre nós
         </a>
       </div>
